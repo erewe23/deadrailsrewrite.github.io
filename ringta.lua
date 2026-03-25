b5d35d19ab28a2b5c11f17fa5c1d6a20789f3a65
erewe23 <eeeiqjjj308@gmail.com>
2026-03-25 18:27:31 -0400

    Update ringta.lua

--- ringta.lua
+++ ringta.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/b8e6859444876fca48ef75188da54f8cbb00982a8fe09a4c603a595afde092d4/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b8e6859444876fca48ef75188da54f8cbb00982a8fe09a4c603a595afde092d4/download"))()
